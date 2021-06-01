aca006a43073dd82e9a462d86c440a8e84945830
MOHAMAD MIRZA <19025376@myrp.edu.sg>
2021-06-01 13:40:39 +0800

    112

--- app/src/main/java/com/myapplicationdev/android/knowyourfacts/Frag1.java
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts/Frag1.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.Random;
 
@@ -16,6 +19,7 @@ import java.util.Random;
 public class Frag1 extends Fragment {
 
     Button buttoncolor;
+    ImageView iv ;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,11 @@ public class Frag1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_frag1, container, false);
+
+        iv = view.findViewById(R.id.iv);
+
+        String imageUrl = "https://wtffunfact.com/wp-content/uploads/2021/05/WTF-Fun-Fact-Presidents-Without-Pets.png";
+        Picasso.with(getContext()).load(imageUrl).into(iv);
 
         buttoncolor = view.findViewById(R.id.btnFrag1);
         buttoncolor.setOnClickListener(new View.OnClickListener() {
